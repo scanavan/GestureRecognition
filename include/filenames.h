e8d44903ac932e8529a5b9cacf532340b8cdbedd
rmalon95 <ryan.p.malon@gmail.com>
2016-06-07 16:31:49 -0400

    Updated filename vectors to be public class members.

--- include/filenames.h
+++ include/filenames.h
@@ -9,14 +9,14 @@ class FileNames {
 	private:
 		tinydir_dir dir;
 		
+	public:
+		void readDir(const char * path);
+		void print();
+		
 		std::vector <std::string> depth;
 		std::vector <std::string> rgb;
 		std::vector <std::string> leap;
 		std::vector <std::string> bin;
-		
-	public:
-		void readDir(const char * path);
-		void print();
 };
 
 #endif
