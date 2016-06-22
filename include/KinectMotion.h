ca509a31cd34f264d6e48311bf76e373e3d2248d
rmalon95 <ryan.p.malon@gmail.com>
2016-06-22 14:38:31 -0400

    Added comment to KinectMotion.h with types for each feature vector

--- include/KinectMotion.h
+++ include/KinectMotion.h
@@ -46,4 +46,14 @@ std::vector<cv::Point> getContour(cv::Mat image);
 void createWindow(cv::Mat image, std::string imageName);
 float * silhouette(cv::Mat image);
 
+/*
+ * FEATURES
+ * 
+ * Silhouette			float[32]
+ * Contour Distances	float[180] 
+ * Cell Occupancy		float[?] - depends on scaled image size
+ * Convex Hull			float[6]
+ * 
+ */
+
 #endif
