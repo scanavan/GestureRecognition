d7eac4ef25eece159282fce7cc518139bc3cf683
rmalon95 <ryan.p.malon@gmail.com>
2016-07-11 15:00:41 -0400

    Faster. Stored contour and palm center in init.

--- source/KinectMotion.cpp
+++ source/KinectMotion.cpp
@@ -10,11 +10,11 @@
 #include <vector>
 #include <set>
 
-KinectMotion::KinectMotion(std::string fleap, std::string fdepth, std::string frgb)
+KinectMotion::KinectMotion(std::string fleap, std::string fdepth)
 {
 	leap = new LeapData(fleap);
 	depth = cv::imread(fdepth, CV_LOAD_IMAGE_UNCHANGED);
-	rgb = cv::imread(frgb, CV_LOAD_IMAGE_UNCHANGED);
+	//rgb = cv::imread(frgb, CV_LOAD_IMAGE_UNCHANGED);
 
 	int index = fdepth.find_last_of("/");
 	char gestureNumber = fdepth.at(index - 1);
@@ -23,27 +23,36 @@ KinectMotion::KinectMotion(std::string fleap, std::string fdepth, std::string fr
 
 	depth = updateImage(depth);
 	depth = rotateImage(depth);
-	getHand(depth, 0.95);
-	depth = scaleHand(depth);
-
-	sil = silhouette(depth);
-	contour_dist = distContour(binarize(depth));
-	hull = hullAreas(depth);
-	Occ occ_data = cellOccupancy(depth);
+	getHand2(depth);
+	initData();
+	sil = silhouette(scaled_depth);
+	contour_dist = distContour(scaled_binary);
+	hull = hullAreas(scaled_depth);
+	Occ occ_data = cellOccupancy(scaled_depth);
 	occ_avg = occ_data.avgD;
 	occ_nonz = occ_data.nonZ;
 
 }
 
+
+void KinectMotion::initData()
+{
+	scaled_depth = scaleHand(depth);
+	scaled_binary = binarize(scaled_depth);
+
+	palm_center = palmCenter(scaled_binary, 150);
+	scaled_contour = getContour(scaled_binary);
+
+}
 cv::Mat KinectMotion::getDepth()
 {
 	return depth;
 }
 
-cv::Mat KinectMotion::getRgb()
-{
-	return rgb;
-}
+//cv::Mat KinectMotion::getRgb()
+//{
+//	return rgb;
+//}
 
 float * KinectMotion::getSil()
 {
@@ -179,7 +188,6 @@ std::vector<cv::Point> KinectMotion::getContour(cv::Mat image)
 	std::vector<cv::Point> rawContour;
 	std::vector<cv::Point> sortedContour;
 	cv::Mat image_clone = binarize(image);
-	cv::Point pc = palmCenter(image_clone, 23);
 
 	std::vector<std::vector<cv::Point>> contours;
 	cv::findContours(image_clone, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
@@ -200,23 +208,29 @@ std::vector<cv::Point> KinectMotion::getContour(cv::Mat image)
 		rawContour = contours[max_index];
 	}
 
+	return rawContour;
+}
+
+std::vector<cv::Point> KinectMotion::getSortedContour(cv::Mat image)
+{
+	std::vector<cv::Point> sortedContour;
+
 	int start = 0;
-	for (int i = 0; i < rawContour.size(); i++)
+	for (int i = 0; i < scaled_contour.size(); i++)
 	{
-		if (rawContour[i].x == pc.x && rawContour[i].y <= pc.y)
+		if (scaled_contour[i].x == palm_center.x && scaled_contour[i].y <= palm_center.y)
 		{
 			start = i;
 			break;
 		}
 	}
-	for (int i = 0; i < rawContour.size(); i++)
+	for (int i = 0; i < scaled_contour.size(); i++)
 	{
-		sortedContour.push_back(rawContour[(i + start) % rawContour.size()]);
+		sortedContour.push_back(scaled_contour[(i + start) % scaled_contour.size()]);
 	}
 
 	return sortedContour;
 }
-
 /*
 @def - scales hand to a fixed square image
 @param - image - image matrix
@@ -234,19 +248,19 @@ cv::Mat KinectMotion::scaleHand(cv::Mat image)
 		height = croppedImage.rows;
 	int max_dim = (width >= height) ? width : height;
 
-	float scale = ((float)SCALE) / max_dim;
+	float scale = ((float)440) / max_dim;
 	cv::Rect roi;
 	if (width >= height)
 	{
-		roi.width = SCALE;
-		roi.x = 0;
+		roi.width = 440;
+		roi.x = 20;
 		roi.height = height * scale;
 		roi.y = (SCALE - roi.height) / 2;
 	}
 	else
 	{
-		roi.y = 0;
-		roi.height = SCALE;
+		roi.y = 20;
+		roi.height = 440;
 		roi.width = width * scale;
 		roi.x = (SCALE - roi.width) / 2;
 	}
@@ -269,8 +283,7 @@ calculates the distance from the palm center to each sampled point
 */
 float * KinectMotion::distContour(cv::Mat image)
 {
-	std::vector<cv::Point> edges = getContour(image);
-	cv::Point center = palmCenter(image, 23);
+	std::vector<cv::Point> edges = getSortedContour(image);
 	std::vector<cv::Point> sampleContour;
 	float * retVal = new float[SAMPLE_SIZE] {0};
 
@@ -282,7 +295,7 @@ float * KinectMotion::distContour(cv::Mat image)
 	float max = 0;
 	for (int i = 0; i < sampleContour.size(); i++)
 	{
-		float temp = std::sqrt(std::pow(center.x - sampleContour.at(i).x, 2) + std::pow(center.y - sampleContour.at(i).y, 2));
+		float temp = std::sqrt(std::pow(palm_center.x - sampleContour.at(i).x, 2) + std::pow(palm_center.y - sampleContour.at(i).y, 2));
 		retVal[i] = temp;
 		if (temp > max)
 		{
@@ -335,10 +348,10 @@ cv::Rect KinectMotion::getRect(cv::Mat image)
 		}
 	}
 
-	maxPoint.x = xMax + 10;
-	maxPoint.y = yMax + 10;
-	minPoint.x = xMin - 10;
-	minPoint.y = yMin - 10;
+	maxPoint.x = xMax;
+	maxPoint.y = yMax;
+	minPoint.x = xMin;
+	minPoint.y = yMin;
 
 	return cv::Rect(maxPoint, minPoint);
 }
@@ -366,17 +379,14 @@ cv::Mat KinectMotion::rotateImage(cv::Mat image)
 */
 float * KinectMotion::silhouette(cv::Mat image)
 {
-	cv::Mat uimage = binarize(image, 11);
-
 	cv::Point center = cv::Point(image.cols / 2, image.rows / 2);
-	std::vector<cv::Point> contour = getContour(uimage);
 	std::vector<float> bins[32];
 
 	float angle, dist;
 	int x, y, bin;
-	for (int i = 0; i < contour.size(); ++i)
+	for (int i = 0; i < scaled_contour.size(); ++i)
 	{
-		x = contour[i].x - center.x; y = center.y - contour[i].y;
+		x = scaled_contour[i].x - center.x; y = center.y - scaled_contour[i].y;
 		angle = atan2(y, x);
 		if (angle < 0) angle = (2 * PI) + angle;
 		dist = sqrt((x*x) + (y*y));
@@ -404,13 +414,10 @@ float * KinectMotion::hullAreas(cv::Mat image)
 {
 	float * ret_array = new float[6]{ 0,0,0,0,0,0 };
 
-	cv::Mat uimage = binarize(image, 11);
-
-	std::vector<cv::Point> og_contour = getContour(uimage);
-	double hand_area = contourArea(og_contour);
+	double hand_area = contourArea(scaled_contour);
 
 	std::vector<cv::Point> hull;
-	convexHull(og_contour, hull);
+	convexHull(scaled_contour, hull);
 
 	cv::Mat hull_image = cv::Mat::zeros(image.size(), CV_8U);
 	std::vector <std::vector<cv::Point>> contours;
@@ -424,14 +431,12 @@ float * KinectMotion::hullAreas(cv::Mat image)
 	{
 		for (int j = 0; j < image.cols; ++j)
 		{
-			if (uimage.at<uchar>(i, j) != 0) hull_image.at<uchar>(i, j) = 0;
+			if (scaled_binary.at<uchar>(i, j) != 0) hull_image.at<uchar>(i, j) = 0;
 		}
 	}
-	cv::Point pc = palmCenter(uimage, 40);
 
-	cv::Mat image_clone = hull_image.clone();
-	cv::Mat new_image = cv::Mat::zeros(image.size(), CV_8UC3);
-	cv::findContours(image_clone, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+	//cv::Mat new_image = cv::Mat::zeros(image.size(), CV_8UC3);
+	cv::findContours(hull_image, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
 	std::vector<std::vector<cv::Point>> thresholded_hull_contours;
 	for (int i = 0; i < contours.size(); ++i)
 	{
@@ -453,10 +458,10 @@ float * KinectMotion::hullAreas(cv::Mat image)
 	std::vector<float> angles;
 	for (int i = 0; i < thresholded_hull_contours.size(); ++i)
 	{
-		cv::drawContours(new_image, thresholded_hull_contours, i, cv::Scalar(i * 255 / thresholded_hull_contours.size(), 0, 255));
+		//cv::drawContours(new_image, thresholded_hull_contours, i, cv::Scalar(i * 255 / thresholded_hull_contours.size(), 0, 255));
 		cv::Moments m = moments(thresholded_hull_contours[i]);
 		cv::Point centroid = cv::Point((int)(m.m10 / m.m00), (int)(m.m01 / m.m00));
-		angles.push_back(atan2(pc.y - centroid.y, centroid.x - pc.x));
+		angles.push_back(atan2(palm_center.y - centroid.y, centroid.x - palm_center.x));
 		if (angles[i] < 0) angles[i] = (2 * PI) + angles[i];
 		ret_array[i] = contourArea(thresholded_hull_contours[i]) / hand_area;
 	}
@@ -556,5 +561,65 @@ Occ KinectMotion::cellOccupancy(cv::Mat image)
 	ret_val.nonZ = nonZs; ret_val.avgD = avgs;
 
 	return ret_val;
+}
+cv::Mat KinectMotion::getHand2(cv::Mat image)
+{
+	int top = 0;
+	bool foundHand = false;
+	int handToWrist = 0;
+	double numPixels = 0;
+	int max = 0;
+	bool atWrist = false;
+	int tmp = 0;
+	double thresholdRatio = 0;
+	double maxThresholdRatio = 0;
+	double previousNumPixels = 0;
+
+	for (int i = 25; i < image.rows; i++)
+	{
+		for (int j = 30; j < image.cols - 30; j++)
+		{
+			if (image.at<uchar>(i, j) != 0)
+			{
+				numPixels++;
+				if (!foundHand)
+				{
+					top = i;
+					foundHand = true;
+				}
+			}
+		}
+		if (foundHand && numPixels != 0) {
+			thresholdRatio = previousNumPixels / numPixels;
+			if (maxThresholdRatio < thresholdRatio) {
+				maxThresholdRatio = thresholdRatio;
+			}
+		}
+		previousNumPixels = numPixels;
+		if (numPixels > 0 && !atWrist)
+		{
+			handToWrist++;
+			if (numPixels > max)
+			{
+				max = numPixels;
+			}
+			if (i > top + 50 && tmp != 0 && numPixels <= tmp * thresholdRatio && tmp <= max*.75)
+			{
+				atWrist = true;
+			}
+			tmp = numPixels;
+			numPixels = 0;
+		}
+	}
+
+	for (int i = (top + handToWrist + 10); i < image.rows; i++)
+	{
+		for (int j = 0; j < image.cols; j++)
+		{
+			image.at<uchar>(i, j) = 0;
+		}
+	}
+
+	return image;
 }
 
