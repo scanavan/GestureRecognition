a549fc5cbe8134afda7c905cc38210f61510a12a
rmalon95 <ryan.p.malon@gmail.com>
2016-07-19 15:06:01 -0400

    New contour distances (for test)

--- source/KinectMotion.cpp
+++ source/KinectMotion.cpp
@@ -238,6 +238,7 @@ float * KinectMotion::distContour(cv::Mat image)
 	std::vector<cv::Point> edges = getSortedContour(image);
 	std::vector<cv::Point> sampleContour;
 	float * retVal = new float[SAMPLE_SIZE] {0};
+	float * new_ret_val = new float[SAMPLE_SIZE]; int max_index;
 
 	for (int i = 0; i < SAMPLE_SIZE; i++)
 	{
@@ -252,6 +253,7 @@ float * KinectMotion::distContour(cv::Mat image)
 		if (temp > max)
 		{
 			max = temp;
+			max_index = i;
 		}
 	}
 
@@ -263,7 +265,13 @@ float * KinectMotion::distContour(cv::Mat image)
 		}
 	}
 
-	return retVal;
+	for (int j = 0; j < SAMPLE_SIZE; j++)
+	{
+		new_ret_val[j] = retVal[j + max_index % SAMPLE_SIZE];
+	}
+
+
+	return new_ret_val;
 }
 
 cv::Rect KinectMotion::getRect(cv::Mat image)
